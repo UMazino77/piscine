dac50eb2611c4321fb7c1613c85092af442841fc
matmani <mohamedatm2003@gmail.com>
2025-07-24 10:57:48 +0100

    tset

--- temperature_conv/target/debug/deps/temperature_conv-bf00cc961c7f3432.d
+++
@@ -1,5 +0,0 @@
-/home/matmani/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-bf00cc961c7f3432.d: src/main.rs
-
-/home/matmani/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-bf00cc961c7f3432.rmeta: src/main.rs
-
-src/main.rs:
--- temperature_conv/target/debug/deps/temperature_conv-fc95402fb05ca3e8.d
+++
@@ -1,5 +0,0 @@
-/home/matmani/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-fc95402fb05ca3e8.d: src/lib.rs
-
-/home/matmani/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-fc95402fb05ca3e8.rmeta: src/lib.rs
-
-src/lib.rs:
