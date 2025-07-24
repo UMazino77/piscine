4b35f3bda43359e8940fd47ae494aae693617cb5
matmani <mohamedatm2003@gmail.com>
2025-07-24 14:15:50 +0100

    the iteration ver of fibonacci

--- fibonacci2/src/lib.rs
+++ fibonacci2/src/lib.rs
@@ -2,10 +2,10 @@ pub fn fibonacci(n: u32) -> u32 {
     let mut a = 0;
     let mut b = 1;
     let mut c = 1;
-    for i in 0..n-1 {
+    for _ in 0..n-1 {
         c = a+b ;
         a = b ;
         b = c ;
     }
-    b
+    c
 }
