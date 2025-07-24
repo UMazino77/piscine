5cabecfccb571ae622b1fdf9a9baf7769a0535af
matmani <mohamedatm2003@gmail.com>
2025-07-24 11:36:14 +0100

    looping riddle

---
+++ looping/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut i = 1;
+    loop {
+        let mut riddle = String::new();
+        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+        io::stdin()
+            .read_line(&mut riddle)
+            .expect("Failed to read input");
+        if riddle == "The letter e" {
+            println!(Number of trials: {i});
+            break ;
+        }
+        i+=1;
+    }
+}
