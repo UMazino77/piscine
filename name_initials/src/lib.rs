d42e1a018867bffd9fec4cf2f28356090ec36419
matmani <mohamedatm2003@gmail.com>
2025-07-25 12:08:38 +0100

    name initials : ===> more of string manip this time with vec

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,7 +1,9 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
     let mut a :Vec<String> = vec![];
     for name in names {
-        let n = name.to_string()[0..1].to_owned() + ("."); 
+        let b = find(name, ' ');
+        let (c,d) = split_at(name, b);
+        let n = c.to_string()[0..1].to_owned() + (".") + &d.to_string()[0..2].to_owned() + ("."); 
         a.push(n);
     }
     a
@@ -9,3 +11,21 @@ pub fn initials(names: Vec<&str>) -> Vec<String> {
 
 // second encounter with vec 
 // switching between to_string() and to_owned()
+
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    let a = v.split_at_checked(index);
+    match a{
+        Some ((b,c)) => return (b,c),
+        None => println!("error") 
+    }
+    ("","")
+}
+
+pub fn find(v: &str, pat: char) -> usize {
+    let a = v.find(pat);
+    match a {
+        Some(a) => return a ,
+        None => println!("error")
+    }
+    v.len()
+}
