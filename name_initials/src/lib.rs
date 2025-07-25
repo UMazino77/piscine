3bf0638332bcf216d2cde892941ec3fce599938f
matmani <mohamedatm2003@gmail.com>
2025-07-25 12:21:03 +0100

    name initials : ===> more of string manip this time with vec &&& tricky heap meomory management

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,31 +1,13 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
     let mut a :Vec<String> = vec![];
     for name in names {
-        let b = find(name, ' ');
-        let (c,d) = split_at(name, b);
-        let n = c.to_string()[0..1].to_owned() + (".") + &d.to_string()[0..2].to_owned() + ("."); 
-        a.push(n);
+        let n: Vec<_> = name.split(' ').collect();
+        let p = n[0][0..1].to_owned()+". "+&n[1][0..1].to_owned()+".";
+        a.push(p);
     }
     a
 }
 
+
 // second encounter with vec 
 // switching between to_string() and to_owned()
-
-pub fn split_at(v: &str, index: usize) -> (&str, &str) {
-    let a = v.split_at_checked(index);
-    match a{
-        Some ((b,c)) => return (b,c),
-        None => println!("error") 
-    }
-    ("","")
-}
-
-pub fn find(v: &str, pat: char) -> usize {
-    let a = v.find(pat);
-    match a {
-        Some(a) => return a ,
-        None => println!("error")
-    }
-    v.len()
-}
