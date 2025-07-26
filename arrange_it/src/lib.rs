5e50f04af7e75e2aa5be9bec617f6438594eb83e
matmani <mohamedatm2003@gmail.com>
2025-07-26 17:32:08 +0100

    arrange phrase : ===> implementing indexed vectors // more of string manip

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,12 +1,30 @@
 pub fn arrange_phrase(phrase: &str) -> String {
     let b = phrase.to_owned();
     let a: Vec<_> = b.split(" ").collect();
+    let mut dd: Vec<String> = vec![String::new(); a.len()];
 
-    let s : String :: String.new();
-    
-    for (ind,val) in a.into_iter().enumerate() {
-        println!("{} ===>  {}",ind, val)
+    // let s = String::from("");
+    let mut i = 0 ;
+    while i < a.len() {
+        // println!("{}", a[i]);
+        // dd[i] = a[i].to_string();
+        let mut cc = 0;
+        let mut jj = String::new();
+        let mut k:String = String::new();
+
+        for l in a[i].chars() {
+            if l.is_ascii_digit() {
+                jj.push(l);
+                continue
+                // a[i].remove(j);
+                // j+=1 ;
+            }
+            k.push(l);
+        }
+        cc = jj.parse().expect("error");
+        dd[cc-1] = k;
+
+        i+=1;
     }
-
-    todo!()
-}
+    dd.join(" ")
+ }
--- arrange_it/src/main.rs
+++ arrange_it/src/main.rs
@@ -2,5 +2,5 @@ use arrange_it::*;
 
 fn main() {
     arrange_phrase("is2 Thi1s T4est 3a");
-    //println!("{}", arrange_phrase("is2 Thi1s T4est 3a"));
+    println!("{}", arrange_phrase("is2 Thi1s T4est 3a"));
 }
