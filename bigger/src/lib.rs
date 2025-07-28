339fa5b069e9187faa36c9579d5967099726617d
matmani <mohamedatm2003@gmail.com>
2025-07-28 18:40:09 +0100

    bigger ===>  another refresher for the hashmap / the oppsosite of min/max in the checkpoint

---
+++ bigger/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+    let a = h.values()
+    let max = i32::MIN;
+    for i in a {
+        if i > max {
+            max = i
+        }
+    }
+    max
+}
