914dbb2716eef595823c97bbc41239aab1da9cda
matmani <mohamedatm2003@gmail.com>
2025-07-28 19:22:52 +0100

    string permutation ===>  another classic in string manip/hashmaps/arrays

---
+++ string_permutation/src/lib.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+pub fn is_permutation(s1: &str, s2: &str) -> bool {
+    if s1.len() != s2.len() {
+        return false ;
+    }
+    let mut j = HashMap::new();
+    let mut k= HashMap::new();
+
+    let bbb = s2.chars().collect::<Vec<_>>();
+
+    for v in bbb.iter() {
+        let kk = j.entry(v).or_insert(0);
+        *kk += 1;
+    }
+
+    let aaa = s2.chars().collect::<Vec<_>>();
+    for v in aaa.iter() {
+        let kk = k.entry(v).or_insert(0);
+        *kk += 1;
+    }
+
+    for val in j.keys() {
+        let a = j.get(val).copied().unwrap_or(0);
+        let b = k.get(val).copied().unwrap_or(0);
+        if a != b {
+            return false;
+        }
+    }
+    true
+}
