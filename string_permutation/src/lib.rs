6174e5c7a4297129a3425a427a8c5e7afd4c0b0c
matmani <mohamedatm2003@gmail.com>
2025-07-28 19:30:44 +0100

    string permutation ===>  another classic in string manip/hashmaps/arrays

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -7,7 +7,7 @@ pub fn is_permutation(s1: &str, s2: &str) -> bool {
     let mut j = HashMap::new();
     let mut k= HashMap::new();
 
-    let bbb = s2.chars().collect::<Vec<_>>();
+    let bbb = s1.chars().collect::<Vec<_>>();
 
     for v in bbb.iter() {
         let kk = j.entry(v).or_insert(0);
@@ -15,14 +15,18 @@ pub fn is_permutation(s1: &str, s2: &str) -> bool {
     }
 
     let aaa = s2.chars().collect::<Vec<_>>();
+
     for v in aaa.iter() {
         let kk = k.entry(v).or_insert(0);
         *kk += 1;
     }
 
+    // println!("{:?} ====> {:?}" ,j, k );
+
     for val in j.keys() {
         let a = j.get(val).copied().unwrap_or(0);
         let b = k.get(val).copied().unwrap_or(0);
+        // println!("{} ===> {}, {}", a, b, val);
         if a != b {
             return false;
         }
--- string_permutation/src/main.rs
+++ string_permutation/src/main.rs
@@ -10,4 +10,47 @@ fn main() {
         word1,
         is_permutation(word, word1)
     );
+    println!("is permutation : ===> {}", is_permutation("avcde", "edbca"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        assert!(is_permutation("abcde", "edbca"));
+        assert!(!is_permutation("avcde", "edbca"));
+        assert!(!is_permutation("cde", "edbca"));
+        assert!(is_permutation("code", "deco"));
+        assert!(!is_permutation("code", "deeco"));
+        assert!(!is_permutation("codde", "deeco"));
+    }
+
+    #[test]
+    fn test_repeating_characters() {
+        assert!(is_permutation("aab", "baa"));
+    }
+
+    #[test]
+    fn test_one_char() {
+        assert!(!is_permutation("a", "b"));
+        assert!(is_permutation("a", "a"));
+    }
+
+    #[test]
+    fn test_empty() {
+        assert!(is_permutation("", ""));
+    }
+
+    #[test]
+    fn test_special_characters() {
+        assert!(is_permutation("!#%@", "@%#!"));
+    }
+
+    #[test]
+    fn test_unicode() {
+        assert!(is_permutation("hello♥", "♥oelhl"));
+        assert!(!is_permutation("♥", "♥♥"));
+    }
 }
