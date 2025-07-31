9038bccbb2f4e775636fdb7e3c55f48d2cc65654
matmani <mohamedatm2003@gmail.com>
2025-07-31 17:25:47 +0100

    unwrap / expect  ===>  error handling in rust

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,23 @@
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
+}
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    match security_level {
+        Security::Unknown => server.unwrap().to_owned(),
+        Security::Message => server.unwrap_or_else(|err| {panic!("ERROR: program stops")    }).to_owned(),
+        Security::Warning => server.expect("WARNING: check the sever").to_owned(),
+        Security::NotFound => {
+            match server {
+                Ok(data) => data.to_owned(),
+                Err(err) => err.to_owned(),
+            }
+        },
+        Security::UnexpectedUrl => server.unwrap_err().to_owned(),
+
+    }
+}
