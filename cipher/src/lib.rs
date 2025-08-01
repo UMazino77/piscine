9a446ecf6ab29f9b98207cc0fda608649f28184c
matmani <mohamedatm2003@gmail.com>
2025-08-01 21:49:28 +0100

    cipher  ===>  ciphering and string manip with results

---
+++ cipher/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug, PartialEq)]
+pub struct CipherError {
+    expected : String,
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
+    let a = original.chars();
+    let mut ab = String::new();
+    for i in a {
+        if i.is_ascii_lowercase() {
+            let c = i as u8 - 97;
+            let cc = 122 - c ;
+            let d = cc as char;
+            ab.push(d);
+        } else if i.is_ascii_uppercase() {
+            let c = i as u8 - 65;
+            let cc = 90 - c ;
+            let d = cc as char;
+            ab.push(d);
+        } else {
+            ab.push(i);
+        }
+    }
+    if ab == ciphered.to_owned() {
+        Ok(())
+    } else {
+        Err(CipherError{expected : ab})
+    }
+}
