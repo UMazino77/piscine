382905124875015cb0e41b76569e324aa74628df
matmani <mohamedatm2003@gmail.com>
2025-08-02 08:17:25 +0100

    macro calculator  ===>  not that interesting of an exercise

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -15,12 +15,13 @@ pub fn calculate_macros(foods: &[Food]) -> json::JsonValue {
     let mut fats = 0.0;
 
     for food in foods {
-        let cal : f64= food.calories.1.trim_end_matches("kcal").parse().expect("error") ;
+        let cal : f64= food.calories.1.trim_end_matches("kcal").parse().unwrap() ;
         cals += cal * food.nbr_of_portions;
         carbs += food.carbs * food.nbr_of_portions ;
         proteins += food.proteins * food.nbr_of_portions ;
         fats += food.fats * food.nbr_of_portions ;
     }
+    
     data["cals"] = helper(cals).into();
     data["carbs"] = helper(carbs).into();
     data["proteins"] = helper(proteins).into();
