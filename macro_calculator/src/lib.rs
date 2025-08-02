0b51022092f0a452e778fb902d030e319e97f47a
matmani <mohamedatm2003@gmail.com>
2025-08-02 07:48:36 +0100

    macro calculator  ===>  not that interesting of an exercise

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -1,14 +1,33 @@
 pub struct Food {
-    name : String,
-    calories: (String, String),
-    fats: f64,
-    carbs: f64,
-    proteins: f64,
-    nbr_of_portions: f64
+    pub name : String,
+    pub calories: (String, String),
+    pub fats: f64,
+    pub carbs: f64,
+    pub proteins: f64,
+    pub nbr_of_portions: f64
 }
 
 pub fn calculate_macros(foods: &[Food]) -> json::JsonValue {
-    todo!()
+    let mut data = json::JsonValue::new_object();
+    let mut cals = 0.0;
+    let mut carbs = 0.0;
+    let mut  proteins = 0.0;
+    let mut fats = 0.0;
+
+    for food in foods {
+        let cal : f64= food.calories.1.parse().expect("error") ;
+        cals += cal * food.nbr_of_portions;
+        carbs += food.carbs * food.nbr_of_portions ;
+        proteins += food.proteins * food.nbr_of_portions ;
+        fats += food.fats * food.nbr_of_portions ;
+    }
+    data["cals"] = helper(cals).into();
+    data["carbs"] = helper(carbs).into();
+    data["proteins"] = helper(proteins).into();
+    data["fats"] = helper(fats).into();
+
+    data
+
 }
 
 fn helper(num: f64) -> String {
