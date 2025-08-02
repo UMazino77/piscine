f5a4f2622e306223a4bf0d5ffbeec72ecfc0f1ea
matmani <mohamedatm2003@gmail.com>
2025-08-02 08:44:39 +0100

    shopping mall  ===>  to do in the evening

---
+++ shopping_mall/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn biggest_store(mall : &Mall) -> Store {
+    todo!()
+}
+
+pub fn highest_paid_employee(mall : &Mall) -> Vec[Employee] {
+    todo!()
+}
+
+
+pub fn nbr_of_employees(&mall : &Mall) -> usize {
+    todo!()
+}
+
+pub fn check_for_securities(mall : mut &Mall, HashMap<String, Guard>) -> usize {
+    todo!()
+}
+
+pub fn cut_or_raise(mall : &Mall) -> usize {
+    todo!()
+}
