fd6422210bd3683af8a99c2e793de4179cc6a75b
matmani <mohamedatm2003@gmail.com>
2025-08-03 03:02:26 +0100

    shopping mall  ===>  that was a long one with not much to learn

--- shopping_mall/src/lib.rs
+++ shopping_mall/src/lib.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 
 pub fn biggest_store(mall : &Mall) -> (&str, &Store) {
     let mut max_area = 0;
-    let mut name = String::new() ;
+    let mut name = "" ;
     let aa : HashMap<String, Employee> = Default::default() ;
     let mut strr = None;
     for floor in mall.floors.values(){
@@ -13,7 +13,7 @@ pub fn biggest_store(mall : &Mall) -> (&str, &Store) {
             if store.square_meters >= max_area {
                 max_area = store.square_meters.clone();
                 strr = Some(&store)  ;
-                name = (*key.clone()) ;
+                name = *key.clone() ;
             }
         }
     }
@@ -66,7 +66,7 @@ pub fn check_for_securities(mall :  &mut Mall,guards :HashMap<String, Guard>) ->
         }
     }
     for (name, guard) in guards {
-            if mall.guards.len()-1 as u64 > area/200 {
+            if mall.guards.len() as u64 > area/200 +1{
                 break;
             }
             mall.hire_guard(name, guard);
