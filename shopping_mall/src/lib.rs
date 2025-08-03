767a90c1ce0013a610798fd0f17bd05c0c30b9ab
matmani <mohamedatm2003@gmail.com>
2025-08-03 02:16:44 +0100

    shopping mall  ===>  that was a long one with not much to learn

--- shopping_mall/src/lib.rs
+++ shopping_mall/src/lib.rs
@@ -1,20 +1,83 @@
+pub mod mall ;
+pub use mall::* ;
+use std::collections::HashMap;
+
+
 pub fn biggest_store(mall : &Mall) -> Store {
-    todo!()
+    let mut max_area = 0;
+    // let empl = Employee {age: 0, working_hours: (0, 0),  salary: 0.0};
+    let aa : HashMap<String, Employee> = Default::default() ;
+    let mut strr = Store::new(aa, 0);
+    for floor in mall.floors.values(){
+        for store in floor.stores.values() {
+            if store.square_meters >= max_area {
+                max_area = store.square_meters;
+                strr = store.clone()  ;
+            }
+        }
+    }
+
+    strr
 }
 
-pub fn highest_paid_employee(mall : &Mall) -> Vec[Employee] {
-    todo!()
+pub fn highest_paid_employee(mall : &Mall) -> Vec<Employee> {
+    let mut max = f64::MIN;
+    let mut bb : Vec<Employee> = vec![] ;
+    for floor in mall.floors.values(){
+        for store in floor.stores.values() {
+            for empl in store.employees.values() {
+                if empl.salary >= max {
+                    if empl.salary == max {
+                        bb.push(*empl) ;
+                    } else {
+                        bb.clear() ;
+                        bb.push(*empl) ;
+                    }
+                    max = empl.salary;
+                }
+            }
+        }
+    }
+
+    bb
 }
 
-
-pub fn nbr_of_employees(&mall : &Mall) -> usize {
-    todo!()
+pub fn nbr_of_employees(mall : &Mall, ) -> usize {
+    let mut a = 0;
+    for floor in mall.floors.values(){
+        for store in floor.stores.values() {
+            a += store.employees.len();
+        }
+    }
+    a + mall.guards.len()
 }
 
-pub fn check_for_securities(mall : mut &Mall, HashMap<String, Guard>) -> usize {
-    todo!()
+pub fn check_for_securities(mall :  &Mall,mut guards :HashMap<String, Guard>) -> usize {
+    let nb = guards.len();
+    let mut area = 0;
+    for floor in mall.floors.values(){
+        for store in floor.stores.values() {
+            area += store.square_meters ;
+        }
+    }
+    if area / (nb as u64) < 200 {
+        guards.insert("mohamed".to_owned(), Guard{age : 22 , years_experience : 1});
+        return nb+1 ;
+    }
+    nb
 }
 
-pub fn cut_or_raise(mall : &Mall) -> usize {
-    todo!()
+pub fn cut_or_raise( mall : &mut Mall) -> usize {
+    for floor in mall.floors.values_mut(){
+        for store in floor.stores.values_mut() {
+            for empl in store.employees.values_mut() {
+                if empl.working_hours.1-empl.working_hours.0 >= 10 {
+                    empl.salary = empl.salary + empl.salary/10.0 ;
+                } else {
+                    empl.salary = empl.salary - empl.salary/10.0 ;
+                }
+            }
+        }
+    }
+    1
 }
