2c18dcaedbad274d61dc58a3b98a9fea2925f329
matmani <mohamedatm2003@gmail.com>
2025-08-03 04:33:44 +0100

    expected variable  ===>  learnt about path

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -3,8 +3,8 @@ use convert_case::{Case, Casing};
 
 pub fn expected_variable(str1 : &str , str2 : &str)-> Option<String> {
 
-    if (!str1.is_case(Case::Camel) && !str2.is_case(Case::Camel)) && (!str1.is_case(Case::Snake) && !str2.is_case(Case::Snake)) && (!str1.is_case(Case::Pascal) && !str2.is_case(Case::Pascal)) {
-        println!("{}", str1);
+    if (str1.is_case(Case::Camel) && !str2.is_case(Case::Camel)) || (str1.is_case(Case::Snake) && !str2.is_case(Case::Snake)) || (str1.is_case(Case::Pascal) && !str2.is_case(Case::Pascal)) {
+        // println!("{}", str1);
         return None ;
     }
 
