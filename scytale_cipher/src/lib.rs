14b216d0cd20844924d10004eff77de47f6202e3
matmani <mohamedatm2003@gmail.com>
2025-08-05 00:36:27 +0100

    scytale_cipher  ===>  not so easy

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn scytale_cipher(message: &str, i: usize) -> String {
-    if message.is_empty() || i >= message.len() {
+    if message.is_empty() || i == 0 || i >= message.len() {
         return message.to_owned() ;
     }
     
@@ -9,22 +9,22 @@ pub fn scytale_cipher(message: &str, i: usize) -> String {
     let ff : Vec<_> = message.chars().collect() ;
     let mut jj = 0 ; 
 
-    let mut bb = false ;
-    let mut ccc = 0;
-    while jj + ccc < message.len() {
-        ss.push(ff[j]) ;
-        if j + i < message.len() {
-            j = j+i ;
-            bb = false;
-            ccc += 1 ;
-        } else {
-            if bb == true {
+    let z = (message.len() as f64/i as f64).ceil() as usize;
+
+    // let mut bb = false ;
+     while jj < i {
+        j = jj;
+        let mut ccc = 0;
+        while ccc < z {
+            if j < message.len() {
+                ss.push(ff[j]);
+            } else {
                 ss.push(' ');
             }
-            j = jj+1;
-            jj+=1 ;
-            bb = true
+            j += i;
+            ccc += 1;
         }
+        jj += 1;
     }
     ss.trim_end().to_owned()
 }
