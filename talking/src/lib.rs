1ca17268ddf1242ab258aad2ebf195119d612637
matmani <mohamedatm2003@gmail.com>
2025-08-04 20:31:21 +0100

    talking  ===>  easy again

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -2,13 +2,13 @@ pub fn talking(text: &str) -> &str {
     if text.trim().is_empty() {
         return "Just say something!";
     }
-    if is_all_upper(text) && text.ends_with("?") {
+    if is_all_upper(text, false) && text.ends_with("?") {
         return "Quiet, I am thinking!"
     }
     if text.ends_with("?") {
         return "Sure."
     }
-    if is_all_upper(text) && text.ends_with("!") {
+    if is_all_upper(text, true) {
         return "There is no need to yell, calm down!"
     }
 
@@ -17,9 +17,11 @@ pub fn talking(text: &str) -> &str {
     "Interesting"
 }
 
-pub fn is_all_upper(text : &str) -> bool {
+pub fn is_all_upper(text : &str, yell : bool ) -> bool {
     for i in text.chars() {
-        if i.is_ascii_alphanumeric() && (!i.is_ascii_uppercase() || i.is_ascii_digit()) {
+        if i.is_ascii_alphanumeric() && (!i.is_ascii_uppercase() && (!yell || !i.is_ascii_digit())) {
+            // println!("{text}");
+            println!("------   {i}  +++++");
             return false ;
         }
     }
