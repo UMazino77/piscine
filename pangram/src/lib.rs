3ffe6658d0ddfeca08f44b7622e052ab404b22fc
matmani <mohamedatm2003@gmail.com>
2025-08-04 19:05:39 +0100

    pangram  ===>  easy again / not a good use for hashset

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,11 +1,11 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 pub fn is_pangram(s: &str) -> bool {
-    let mut a = HashMap::new() ;
+    let mut a = HashSet::new() ;
     for i in s.chars() {
-        if i.is_alphabetic() {
-            a.insert(i.to_ascii_lowercase(), 1);
+        if i.is_ascii_alphabetic() {
+            a.insert(i.to_ascii_lowercase());
         }
     }
-    // println!("{}", a.len());
+    // println!("{:?}", a);
     return a.len() == 26
 }
