3cfedd6a56b07b9516f28d52c2f51ca174761ead
matmani <mohamedatm2003@gmail.com>
2025-08-04 18:26:06 +0100

    ordinal  ===>  easy again

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn num_to_ordinal(x: u32) -> String {
-    if x%100 != 11 && x%100 != 12 x%100 != 13 {
+    if x%100 != 11 && x%100 != 12 && x%100 != 13 {
         return format!("{}{}", x, "th");
     } 
     if (x-1)%10 == 0 { return format!("{}{}", x, "st")} 
