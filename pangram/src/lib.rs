25950fbf0374d5a808bfb183d798385210e949b6
matmani <mohamedatm2003@gmail.com>
2025-08-04 18:59:41 +0100

    pangram  ===>  easy again / not a good use for hashset

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -3,7 +3,7 @@ pub fn is_pangram(s: &str) -> bool {
     let mut a = HashMap::new() ;
     for i in s.chars() {
         if i.is_alphabetic() {
-            a.insert(i.to_ascii_lowercase());
+            a.insert(i.to_ascii_lowercase(), 1);
         }
     }
     // println!("{}", a.len());
