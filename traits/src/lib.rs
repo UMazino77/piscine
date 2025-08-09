476bfc51e474eabcfc04ae0322101f4fa8e5cc06
matmani <mohamedatm2003@gmail.com>
2025-08-09 17:54:37 +0100

    easy traits  ===>  to finish tomorrow

---
+++ traits/src/lib.rs
@@ -0,0 +1,41 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub struct Player {
+	pub name: String,
+	pub strength: f64,
+	pub score: i32,
+	pub money: i32,
+	pub weapons: Vec<String>,
+}
+
+pub struct Fruit {
+	pub weight_in_kg: f64,
+}
+
+pub struct Meat {
+	pub weight_in_kg: f64,
+	pub fat_content: f64,
+}
+
+impl Player {
+	pub fn eat<T: Food + Display>(&mut self, food: T) {
+		self.strength += food.gives();
+	}
+}
+
+pub trait Food {
+	fn gives(&self) -> f64;
+}
+
+impl Food for Fruit {
+    fn gives(&self) -> f64 {
+        self.weight_in_kg.round()*4.
+    }
+}
+
+impl Food for Meat {
+    fn gives(&self) -> f64 {
+        self.fat_content.round()*9. + (self.weight_in_kg-self.fat_content).round()*4.
+    }
+}
