46af3d67a8e6d07559c73c683d534ec5a35a8a17
matmani <mohamedatm2003@gmail.com>
2025-08-10 03:47:26 +0100

    roman numbers  ===>  to finish later

---
+++ roman_numbers/src/lib.rs
@@ -0,0 +1,23 @@
+use crate::RomanDigit::*;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum RomanDigit {
+	Nulla,
+	I,
+	V,
+	X,
+	L,
+	C,
+	D,
+	M,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RomanNumber(pub Vec<RomanDigit>);
+
+impl From<u32> for RomanDigit {
+    
+}
+
+impl From<u32> for RomanNumber {
+}
