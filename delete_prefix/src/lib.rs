d361b75774d33360392f3330471c0c66cc30a6bc
matmani <mohamedatm2003@gmail.com>
2025-08-15 00:13:09 +0100

    delete prefix  ===>  easy

--- delete_prefix/src/lib.rs
+++ delete_prefix/src/lib.rs
@@ -1,4 +1,10 @@
 pub fn delete_prefix<'a>(prefix: &'a str, s: &'a str) -> Option<&'a str> {
+    if prefix.is_empty() {
+        return Some(s);
+    }
+    if s.len() < prefix.len() {
+        return None;
+    }
     if s[0..prefix.len()] == *prefix {
         return Some(&s[prefix.len()..]) ;
     }
