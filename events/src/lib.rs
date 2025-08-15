a6aa04cab28a8908f8cab275dd867f6a23d9a5ef
matmani <mohamedatm2003@gmail.com>
2025-08-15 01:31:15 +0100

    events  ===>  that was abit fun

--- events/src/lib.rs
+++ events/src/lib.rs
@@ -1,5 +1,7 @@
 use chrono::Duration;
 use colored::*;
+use std::{fmt, str::FromStr};
+use std::fmt::Display;
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Position {
@@ -24,14 +26,52 @@ pub enum Event<'a> {
 	Holiday,
 }
 
-use std::fmt;
 
 impl fmt::Display for Notification {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		writeln!(f, "({}, {}, {})",
+			self.position.to_string(), self.size, self.content.truecolor(self.color.0,self.color.1,self.color.2))
+	}
 }
 
-use Event::*;
+impl ToString for Position {
 
-impl Event {
+	fn to_string(&self) -> String {
+		match self {
+			Position::Top => "Top".to_string(),
+			Position::Bottom => "Bottom".to_string(),
+			Position::Center => "Center".to_string(),
+		}
+	}
+}
+
+impl<'a> Event <'_> {
 	pub fn notify(&self) -> Notification {
+		match self {
+			&Event::Remainder(content) => Notification {
+				size: 50,
+				color: (50, 50, 50),
+				position: Position::Bottom,
+				content:  content.to_string(),
+			},
+			&Event::Appointment(content) => Notification {
+				size: 100,
+				color: (200, 200, 3),
+				position: Position::Center,
+				content: format!("{content}"),
+			},
+			&Event::Registration(duration) => Notification {
+				size: 30,
+				color: (255, 2, 22),
+				position: Position::Top,
+				content: format!("You have {}H:{}M:{}S  left before the registration ends", duration.num_hours(),duration.num_minutes() % 60, duration.num_seconds() % 60),
+			},
+			Event::Holiday => Notification {
+				size: 25,
+				color: (0, 255, 0),
+				position: Position::Top,
+				content: "Enjoy your holiday".to_string(),
+			},
+		}
 	}
 }
